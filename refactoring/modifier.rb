d88cb85e78e1333f49809fab04e692263de2613a
Alexander Paramonov <alexander.n.paramonov@gmail.com>
2015-07-20 17:30:19 +0300

    Add test for reference match

--- refactoring/modifier.rb
+++ refactoring/modifier.rb
@@ -177,11 +177,3 @@ class Modifier
     return output
   end
 end
-
-# modified = input = latest('project_2012-07-27_2012-10-10_performancedata')
-# modification_factor = 1
-# cancellaction_factor = 0.4
-# modifier = Modifier.new(modification_factor, cancellaction_factor)
-# modifier.modify(modified, input)
-
-# puts "DONE modifying"
--- refactoring/spec/modifier_spec.rb
+++ refactoring/spec/modifier_spec.rb
@@ -5,14 +5,25 @@ require 'pry'
 
 describe Modifier do
   include SpecHelpers::Fixtures
+  let(:tmp_dir) { File.expand_path('../tmp', File.dirname(__FILE__)) }
+  let(:report_path) { fixture_path('report.csv') }
+
+  let(:modified_path) { "#{ tmp_dir }/modified.csv" }
+  let(:modified_output) { CSV.read "#{ modified_path }_0.txt", headers: true }
+  let(:reference_modified_output) { CSV.read fixture_path('report.csv_0.txt.reference'), headers: true }
+
+  let(:sorted_output) { CSV.read "#{ report_path }.sorted", headers: true }
+  let(:reference_sorted_output) { CSV.read fixture_path('report.csv.sorted.reference'), headers: true }
+
 
   it "works" do
-    modified = input = fixture_path('report.csv')
+    input = report_path
     modification_factor = 1
     cancellaction_factor = 0.4
     modifier = Modifier.new(modification_factor, cancellaction_factor)
-    modifier.modify(modified, input)
+    modifier.modify(modified_path, input)
 
-    puts "DONE modifying"
+    expect(sorted_output).to eq reference_sorted_output
+    expect(modified_output).to eq reference_modified_output
   end
 end
